5541625bf28d03be34de330a8acd3e166988a7e6
dejavuog <37196535+dejavuog@users.noreply.github.com>
2018-03-09 11:48:28 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,52 @@
+#Bot by DejaVu
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import random
+import os
+
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+    print("ready when you are")
+    print("i am running on " + bot.user.name)
+    print ("with the ID: " + bot.user.id)
+
+@bot.command(pass_context=True)
+async def ping(ctx):
+    await bot.say(":ping_pong: ping!! xSSS")
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    await bot.say("the user name is: {}".format(user.name))
+    await bot.say("the users ID is: {}".format(user.id))
+
+@bot.command(pass_context=True)
+async def embed(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="This Nigga.", color=0x00ff00)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="Highest Role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)    
+
+@bot.command(pass_context=True)
+async def rimg(ctx):
+    imgList = os.listdir("C:/Users/Roberts/Desktop/ok")
+    imgString = random.choice(imgList)
+    path = "C:/Users/Roberts/Desktop/ok/" + imgString
+    print(type(path))
+    await bot.upload(path)
+
+import discord
+import asyncio
+
+@bot.command()
+async def repeat(times : int, content=None):
+    for i in range(times):
+        await bot.say(content)
+
+bot.run("process.env.BOT_TOKEN")
